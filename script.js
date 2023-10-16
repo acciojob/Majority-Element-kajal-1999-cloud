0b4d3eb48253c2cf8e46707e0f2b9d57ff24a869
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-16 11:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your code here
+
+
+
+function majorityElement(nums) {
+    let majorityElement = nums[0];
+    let count = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === majorityElement) {
+            count++;
+        } else if (count === 0) {
+            majorityElement = nums[i];
+            count = 1;
+        } else {
+            count--;
+        }
+    }
+
+    // At this point, majorityElement is a potential majority element.
+    // We need to verify if it appears more than n/2 times.
+
+    count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === majorityElement) {
+            count++;
+        }
+    }
+
+    if (count > Math.floor(nums.length / 2)) {
+        return majorityElement;
+    }
+}
+
+// Example usage
+const inputArray = [2, 1, 2];
+const result = majorityElement(inputArray);
+console.log('Majority Element:', result); // Output: Majority Element: 2
+
